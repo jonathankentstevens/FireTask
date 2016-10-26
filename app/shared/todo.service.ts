4d4e732236af52acf12cb61924a576c9d5c5cc6f
Jonathan K. Stevens <jonathanstevens89@gmail.com>
2016-10-26 08:49:46 -0600

    update

--- app/shared/todo.service.ts
+++ app/shared/todo.service.ts
@@ -32,46 +32,4 @@ export class TodoService {
     removeTodo(todo:Todo) {
         this.todos.remove(todo);
     }
-
-    // Simulate POST /todos
-    // addTodo(todo: Todo): TodoService {
-    //   if (!todo.id) {
-    //     todo.id = ++this.lastId;
-    //   }
-    //   this.todos.push(todo);
-    //   return this;
-    // }
-
-    // Simulate DELETE /todos/:id
-    // deleteTodoById(id: number): TodoService {
-    //   this.todos = this.todos
-    //     .filter(todo => todo.id !== id);
-    //   return this;
-    // }
-
-    // Simulate PUT /todos/:id
-    // updateTodoById(id: number, values: Object = {}): Todo {
-    //   let todo = this.getTodoById(id);
-    //   if (!todo) {
-    //     return null;
-    //   }
-    //   Object.assign(todo, values);
-    //   return todo;
-    // }
-
-    // Simulate GET /todos/:id
-    // getTodoById(id: number): Todo {
-    //   return this.todos
-    //     .filter(todo => todo.id === id)
-    //     .pop();
-    // }
-
-    // Toggle todo complete
-    // toggleTodoComplete(todo: Todo){
-    //   let updatedTodo = this.updateTodoById(todo.id, {
-    //     complete: !todo.complete
-    //   });
-    //   return updatedTodo;
-    // }
-
 }
